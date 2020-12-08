66550905591b964016b5839cd23721bc4e375538
Raul M <rauloctaviomurcia@gmail.com>
2020-12-08 01:48:34 -0700

    validate instructor fields

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,8 @@
 class Instructor < ApplicationRecord
   belongs_to :efac, inverse_of: :instructors
   has_one_attached :resume
+
+  validates :name, presence: true
+  validates :experience, presence: true
+  validates :resume, presence: true
 end
