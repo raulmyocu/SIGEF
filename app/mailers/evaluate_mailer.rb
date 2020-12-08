531994860ab04d2a91a608f37dcf8591d19e603f
ximenasandoval <ximena.sandoval.dh@gmail.com>
2020-12-07 21:33:57 -0700

    :bug: Arreglando correos y vistas

--- app/mailers/evaluate_mailer.rb
+++ app/mailers/evaluate_mailer.rb
@@ -6,9 +6,10 @@ class EvaluateMailer < ApplicationMailer
     @event = event
     mail(to: @user.email, subject: 'Actualización sobre su solicitud en SIGEF')
   end
-  def reject_email(user, event)
+  def reject_email(user, event, reason)
     @user = user
     @event = event
+    @reason = reason
     mail(to: @user.email, subject: 'Actualización sobre su solicitud en SIGEF')
   end
 end
