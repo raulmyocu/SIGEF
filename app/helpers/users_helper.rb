fccd7c7c3544c2914f6d5998cdb59856a2d66227
Raul M <rauloctaviomurcia@gmail.com>
2020-12-08 02:10:52 -0700

    roles in spanish (español ñ.ñ)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def get_user_role role
+    user_role = {
+      admin: 'Administrador',
+      instance: 'Instancia',
+      responsible: 'Responsable',
+      instructor: 'Instructor'
+    }
+    user_role[role.to_sym]
+  end
+
 end
