54aa255e466b728b6e3f7810e80f2f56e5f38b12
Raul M <rauloctaviomurcia@gmail.com>
2020-12-10 15:55:41 -0700

    User delete efacs dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   enum permission: [ :admin, :instance, :responsible, :instructor ]
 
-  has_many :efacs
+  has_many :efacs, dependent: :destroy
 
   def role
     # Nombre en español del tipo de usuario
